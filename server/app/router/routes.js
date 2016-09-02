d3dcba56e2c2506c5b74f6d9d8331b123d78d6a8
Vishal Patel <vis8051@gmail.com>
2016-09-02 10:32:20 +1000

    * Some more features

--- server/app/router/routes.js
+++ server/app/router/routes.js
@@ -1,4 +1,18 @@
-module.exports = function (express,app,fs,os,formidable) {
+module.exports = function (express,app,fs,os,formidable,gm,knoxClient,mongoose,io) {
+
+    var Socket;
+
+    io.on('connection', function (socket) {
+        Socket = socket;
+    })
+
+    var singleImage = new mongoose.Schema({
+        filename: String,
+        votes:Number
+    });
+
+    var singleImageModal = mongoose.model('singleImage',singleImage);
+
     var router = express.Router();
 
     router.get('/', function (req,res,next) {
@@ -36,11 +50,53 @@ module.exports = function (express,app,fs,os,formidable) {
 
             newForm.on('end', function () {
                 fs.rename(tempfile,nfile, function () {
+                    gm(nfile).resize(300).write(nfile,function () {
 
+                        fs.readFile(nfile,function (err,buf) {
+                            var req = knoxClient.put(fname,{
+                                'Content-length': buf.length,
+                                'Content-Type': 'image/jpeg'
+                            });
+
+                            req.on('response', function (res) {
+                                if ( res.statusCode == 200 ){
+                                    var newImage = new singleImageModal({
+                                        filename: fname,
+                                        votes:0
+                                    }).save();
+
+                                    Socket.emit('status', {'msg':'Saved !!', 'delay':3000});
+                                    Socket.emit('doUpdate', {});
+
+                                    fs.unlink(nfile,function () {
+                                        console.log('Local file delted');
+                                    })
+                                }
+                            });
+
+                            req.end(buf);
+                        });
+
+                    });
                 });
             })
 
     });
+
+
+    router.get('/getimages',function (req,res,next) {
+        singleImageModal.find({},function (err,result) {
+            res.send(JSON.stringify(result));
+        })
+    });
+
+
+    router.get('/voteup/:id',function (req, res, next) {
+        singleImageModal.findOneAndUpdate(req.params.id, {$inc:{votes:1}}, function (err, result) {
+            //res.send(200, {votes : result.votes});
+            res.status(200).send({votes : result.votes});
+        })
+    })
 
     app.use('/',router);
 }
--- server/app/server.js
+++ server/app/server.js
@@ -4,6 +4,9 @@ var path = require("path");
 var fs = require('fs');
 var os = require('os');
 var formidable = require('formidable');
+var Config = require('./config/development.json');
+var mongoose = require('mongoose').connect(Config.dbURL);
+var gm = require('gm');
 
 var app = express();
 
@@ -20,17 +23,16 @@ app.use(function (req,res,next) {
 app.set('view engine', 'html');
 app.use(express.static('builds/client-build'));
 
-require('./router/routes')(express,app,fs,os,formidable);
-
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var Config = require('./config/development.json');
 
 var knoxClient = knox.createClient({
     key: Config.S3AccessKey,
     secret: Config.S3SecretKey,
     bucket: Config.S3BucketKey
 });
+
+require('./router/routes')(express,app,fs,os,formidable,gm, knoxClient,mongoose,io);
 
 server.listen(PORT, function () {
     console.log('Photogrid is running on ' + PORT);
